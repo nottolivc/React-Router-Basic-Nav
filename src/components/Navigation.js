064ea32c4a2327187f2f480a6712fe6667c5c293
Andrew Nottoli <nottolivc@gmail.com>
2019-09-04 18:13:37 -0700

    set up react browser router in index and route path in nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -6,12 +7,15 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
+        <Route exact path="/" component={Home} />
           <a href="">Home</a>
         </div>
         <div>
+        <Route exact path="/" component={About} />
           <a href="">About</a>
         </div>
         <div>
+        <Route exact path="/" component={Contact} />
           <a href="">Contact</a>
         </div>
       </div>
